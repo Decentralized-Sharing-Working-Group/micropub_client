150f219bd37186be649cbb22a619c0887c6a192d
Gara64 <paul@cozycloud.cc>
2015-06-25 16:50:41 +0200

    Add token exchange with endpoint

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
 var express = require('express'), 
  path = require('path'),
  http = require('http'), 
+ querystring = require('querystring'),
  htmlparser = require("htmlparser2");
  request = require('request'),
+ FormUrlencoded = require('form-urlencoded'),
  bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080,
  host = process.env.HOST || "127.0.0.1", 
  serverAddress = 'http://' + host + ':' + port;
-var authorizationEndpoint;
+
+var params = {
+	
+	me: null,
+	redirect_uri: serverAddress + '/callback_indieauth',
+	client_id: serverAddress,
+	scope: 'post',
+	response_type: 'code',
+	state: '123456',
+	code: null,
+	token: null
+};
+
+var endpoints = {
+	authorizationEndpoint: null, 
+	micropubEndpoint: null,
+	tokenEndpoint: null
+};
+
 var app = express();
 app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({     
 	extended: true
 })); 
 
-
+/* Endpoint discovery */
 var parser = new htmlparser.Parser({
 	onopentag: function(name, attribs){
         if(name === "link" && attribs.rel === "authorization_endpoint") {
-            authorizationEndpoint = attribs.href;
-            console.log('authorization endpoint : ' + authorizationEndpoint);
+            endpoints.authorizationEndpoint = attribs.href;
+        }
+        else if(name === "link" && attribs.rel === "micropub") {
+        	endpoints.micropubEndpoint = attribs.href;
+        }
+        else if(name === "link" && attribs.rel === "token_endpoint") {
+        	endpoints.tokenEndpoint = attribs.href;
         }
     },
 }, {decodeEntities: true});
@@ -30,6 +55,7 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+/* Queries the authorization endpoint */
 app.get('/auth', function(req, res) {
 	var url = req.query.me;
 	console.log('url : ' + url);
@@ -37,13 +63,15 @@ app.get('/auth', function(req, res) {
 		if (!error && response.statusCode == 200) {
 			parser.write(body);
 			parser.end();
-			if(authorizationEndpoint) {
-				var params = '?me=' + url + 
-								'&client_id='+ serverAddress + 
-								'&redirect_uri=' + serverAddress + '/callback_indieauth' +
-								'&response_type=code' + 
-								'&scope=post';
-				res.redirect(authorizationEndpoint + params);
+			if(endpoints.authorizationEndpoint) {
+				params.me = url;
+				var p = '?me=' + params.me + 
+						'&client_id='+ params.client_id + 
+						'&redirect_uri=' + params.redirect_uri +
+						'&response_type=' + params.response_type +  
+						'&state=' + params.state + 
+						'&scope=' + params.scope;
+				res.redirect(endpoints.authorizationEndpoint + p);
 			}
 			else
 				res.render('index', {error: 'Error'});
@@ -54,30 +82,85 @@ app.get('/auth', function(req, res) {
 	});
 });
 
+/* The authorization endpoint redirects here */
 app.get('/callback_indieauth', function(req, res) {
 	console.log('redirect ok from ' + req.originalUrl);
 
+	params.code = req.query.code;
+
 	request.post({
-		url: authorizationEndpoint,
+		url: endpoints.authorizationEndpoint,
 		form: {
-			code: req.query.code,
-		  	redirect_uri: serverAddress + '/callback_indieauth',
-		  	client_id: serverAddress,
+			code: params.code,
+		  	redirect_uri: params.redirect_uri ,
+		  	client_id: params.client_id,
+		  	state: params.state
 		}},
 		function(err, response, body) {
 			if (!err && response.statusCode == 200) {
 				console.log('server response : ' + body);
-				res.render('callback', {auth: true});
+				getToken(endpoints.tokenEndpoint, params, function(token) {
+					params.token = token;
+					console.log('token : ' + token);
+				});
+				res.render('sender', {status: 'Authentication successful'});
+				
 			}
 			else {
-				console.log('error : ' + err);
 				console.log('server response : ' + body);
-				res.render('callback', {auth: false});
+				res.render('sender', {status: 'Authentication failed'});
 			}
 		}
 	);
-	
 });
+
+/* Micropub route */
+app.post('/send', function(req, res) {
+	console.log('note : ' + req.body.note);
+
+	request.post({
+		url: endpoints.micropubEndpoint,
+		form: {
+			h: 'entry',
+			content: req.body.note
+		},
+		headers: {
+    		'Authorization': 'Bearer ' + params.token
+  		}
+		}, function(err, response, body) {
+			if (!err && response.statusCode == 302) {
+				res.render('sender', {status: 'Note sent'});
+			}
+			else {
+				console.log('code : ' + response.statusCode);
+				console.log('response : ' + JSON.stringify(response));
+				res.render('sender', {status: 'Sending failure'});
+			}
+		}
+	);
+
+});
+
+
+var getToken = function(endpoint, parameters, callback) {
+
+	request.post({
+		url: endpoint,
+		form: {
+			me: params.me,
+			code: params.code,
+			redirect_uri: params.redirect_uri,
+			client_id: params.client_id,
+			state: params.state,
+			scope: params.scope
+		}}, 
+		function(err, response, body) {
+			var parse = querystring.parse(body);
+			var token = parse['access_token'];
+			callback(token);
+		}
+	);
+};
 
 
 var server = http.createServer(app).listen(port, host, function() {
